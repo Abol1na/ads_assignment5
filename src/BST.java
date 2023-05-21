023c624d2a609855df93a21dcaafd3d6d71570a1
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-05-21 23:36:32 +0600

    test and fix

--- src/BST.java
+++ src/BST.java
@@ -14,23 +14,23 @@ public class BST<K extends Comparable<K>, V> {
             this.value = value;
         }
     }
-    public void put(K key, V value) {
+    public void put(K key, V value){
         root = put(root, key, value);
     }
-
     private Node put(Node node, K key, V value) {
         if (node == null) {
+            size++;
             return new Node(key, value);
         }
 
         int cmp = key.compareTo(node.key);
-        if (cmp < 0) {
+        if (cmp < 0)
             node.left = put(node.left, key, value);
-        } else if (cmp > 0) {
+        else if (cmp > 0)
             node.right = put(node.right, key, value);
-        } else {
-            node.value = value;  // Update the value if the key already exists
-        }
+        else
+            node.value = value;
+
         return node;
     }
     public V get(K key) {
@@ -49,48 +49,55 @@ public class BST<K extends Comparable<K>, V> {
         else
             return node;
     }
-    public void delete(K key) {
+    public void delete(K key){
         root = delete(root, key);
     }
-
     private Node delete(Node node, K key) {
         if (node == null)
             return null;
 
         int cmp = key.compareTo(node.key);
-        if (cmp < 0) {
+        if (cmp < 0)
             node.left = delete(node.left, key);
-        } else if (cmp > 0) {
+        else if (cmp > 0)
             node.right = delete(node.right, key);
-        } else {
+        else {
             if (node.left == null)
                 return node.right;
             else if (node.right == null)
                 return node.left;
             else {
-                Node temp = node;
-                node = findMin(temp.right);
-                node.right = deleteMin(temp.right);
-                node.left = temp.left;
+                Node minNode = findMin(node.right);
+                node.key = minNode.key;
+                node.value = minNode.value;
+                node.right = deleteMin(node.right);
             }
         }
+
         return node;
     }
-
 
     private Node findMin(Node node) {
         if (node.left == null)
             return node;
         return findMin(node.left);
     }
-
     private Node deleteMin(Node node) {
         if (node.left == null)
             return node.right;
         node.left = deleteMin(node.left);
         return node;
     }
+    public int size() {
+        return size;
+    }
 
+
+    public Iterator<NodeData<K, V>> iterator() {
+        List<NodeData<K, V>> nodes = new ArrayList<>();
+        inOrderTraversal(root, nodes);
+        return nodes.iterator();
+    }
 
     private void inOrderTraversal(Node node, List<NodeData<K, V>> nodes) {
         if (node == null)
@@ -99,12 +106,6 @@ public class BST<K extends Comparable<K>, V> {
         inOrderTraversal(node.left, nodes);
         nodes.add(new NodeData<>(node.key, node.value));
         inOrderTraversal(node.right, nodes);
-    }
-
-    public Iterator<NodeData<K, V>> iterator() {
-        List<NodeData<K, V>> nodes = new ArrayList<>();
-        inOrderTraversal(root, nodes);
-        return nodes.iterator();
     }
 
 
@@ -125,7 +126,5 @@ public class BST<K extends Comparable<K>, V> {
             return value;
         }
     }
-    public int size() {
-        return size;
-    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
+        BST<Integer, String> bst = new BST<>();
 
+        bst.put(5, "Apple");
+        bst.put(2, "Banana");
+        bst.put(7, "Orange");
+        bst.put(1, "Grapes");
+        bst.put(4, "Mango");
+        bst.put(6, "Pineapple");
+        bst.put(8, "Watermelon");
 
+        System.out.println("Size of the tree: " + bst.size());
+
+        // Retrieve values using keys
+        System.out.println("Value at key 2: " + bst.get(2));
+        System.out.println("Value at key 7: " + bst.get(7));
+
+        bst.delete(4);
+
+        System.out.println("Size after deletion: " + bst.size());
+
+        Iterator<BST.NodeData<Integer, String>> iterator = bst.iterator();
+        while (iterator.hasNext()) {
+            BST.NodeData<Integer, String> nodeData = iterator.next();
+            System.out.println("Key: " + nodeData.getKey() + ", Value: " + nodeData.getValue());
+        }
     }
 }
